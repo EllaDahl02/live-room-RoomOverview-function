4223a492e8627a902559d9aa6776b1ed519a57ca
EllaDahl02 <150259497+EllaDahl02@users.noreply.github.com>
2024-07-01 14:24:50 +0200

    Update BuildningCell.svelte

--- frontend/src/components/BuildningCell.svelte
+++ frontend/src/components/BuildningCell.svelte
@@ -20,7 +20,7 @@
 <a href="building/{building.id}" class="bg-dark/20 rounded-md w-full h-[140px] hover:opacity-80 overflow-clip relative transition { building.id !== "4" ? "saturate-[0.15]":"" }">
   <div class="gradient w-full h-full absolute top-0 bottom-0 left-0 right-0 bg-dark/50 flex flex-col p-2">
     <Spacer />
-    <h4 class="font-extrabold text-white text-[1rem] leading-none">{building.name} - {availableRooms} "2available</h4>
+    <h4 class="font-extrabold text-white text-[1rem] leading-none">{building.name} - {availableRooms} available</h4>
     <h4 class="font-light text-white text-smallContent">{building.address}</h4>
   </div>
   <img src={urls[Math.round(Math.random() * (urls.length - 1))]} alt="Building" class="w-full h-full object-cover" />
